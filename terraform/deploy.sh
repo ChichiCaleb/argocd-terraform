995e5e39a3c2910e6dda3ab6ce04d1fab2c8a43b
chichi <chichi@DESKTOP-PRSG3OL>
2024-02-17 10:24:16 -0800

    add kustomize

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -9,6 +9,7 @@ fi
 env=$1
 echo "Deploying $env with "workspaces/${env}.tfvars" ..."
 
+set -uo pipefail
 set -x
 
 terraform workspace new $env
@@ -17,3 +18,26 @@ terraform init -reconfigure
 export GODEBUG=asyncpreemptoff=1
 export TF_REGISTRY_CLIENT_TIMEOUT=20000
 terraform apply -var-file="workspaces/${env}.tfvars" -auto-approve
+
+set +x
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOTDIR="$(cd ${SCRIPTDIR}/../..; pwd )"
+[[ -n "${DEBUG:-}" ]] 
+
+TMPFILE=$(mktemp)
+terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
+
+if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
+  source "$TMPFILE"
+
+certificate_arn=$(kubectl get secret -n argocd -l argocd.argoproj.io/secret-type=cluster -o json | jq -r '.items[0].metadata.annotations.aws_certificate_arn')
+
+echo CERTIFICATE_ARN=$certificate_arn > ../k8s/apps/guestbook/base/environment-properties.env
+
+kubectl apply -k  ../k8s/apps/guestbook/environments/$env 
+
+fi
+
+
+
