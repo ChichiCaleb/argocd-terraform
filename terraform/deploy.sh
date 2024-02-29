435eb58ec4f3749d11545034cfee1bd5fa196553
chichi <chichi@DESKTOP-PRSG3OL>
2024-02-29 07:02:36 -0800

    kustomize debug

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -22,29 +22,29 @@ export TF_REGISTRY_CLIENT_TIMEOUT=20000
 terraform init -reconfigure 
 terraform apply -var-file="workspaces/${env}.tfvars" -auto-approve
 
-# set +x
+set +x
 
-# SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# ROOTDIR="$(cd ${SCRIPTDIR}/../..; pwd )"
-# [[ -n "${DEBUG:-}" ]] 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOTDIR="$(cd ${SCRIPTDIR}/../..; pwd )"
+[[ -n "${DEBUG:-}" ]] 
 
-# TMPFILE=$(mktemp)
-# terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
+TMPFILE=$(mktemp)
+terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
 
-# if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-#   source "$TMPFILE"
+if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
+  source "$TMPFILE"
 
-# certificate_arn=$(kubectl get secret -n argocd -l argocd.argoproj.io/secret-type=cluster -o json | jq -r '.items[0].metadata.annotations.aws_certificate_arn')
-# secret_identifier=$(kubectl get secret -n argocd -l argocd.argoproj.io/secret-type=cluster -o json | jq -r '.items[0].metadata.annotations.workload_sm_secret')
-# region=$(:.aws_region')
+certificate_arn=$(kubectl get secret -n argocd -l argocd.argoproj.io/secret-type=cluster -o json | jq -r '.items[0].metadata.annotations.aws_certificate_arn')
+secret_identifier=$(kubectl get secret -n argocd -l argocd.argoproj.io/secret-type=cluster -o json | jq -r '.items[0].metadata.annotations.workload_sm_secret')
+region=$(kubectl get secret -n argocd -l argocd.argoproj.io/secret-type=cluster -o json | jq -r '.items[0].metadata.annotations.aws_region')
 
-# echo CERTIFICATE_ARN=$certificate_arn > ../k8s/apps/guestbook/base/environment-properties.env
-# echo SECRET_IDENTIFIER=$secret_identifier > ../k8s/apps/guestbook/base/environment-properties.env
-# echo REGION=$region > ../k8s/apps/guestbook/base/environment-properties.env
+echo CERTIFICATE_ARN=$certificate_arn > ../k8s/apps/guestbook/base/environment-properties.env
+echo SECRET_IDENTIFIER=$secret_identifier > ../k8s/apps/guestbook/base/environment-properties.env
+echo REGION=$region > ../k8s/apps/guestbook/base/environment-properties.env
 
-# kubectl apply -k  ../k8s/apps/guestbook/environments/$env
+kubectl apply -k  ../k8s/apps/guestbook/environments/$env
 
-# fi
+fi
 
 
 
